ca51aa80694bdb4b36e62aa079938b1dababfd9c
G-Lumia <giorgiolumia@gmail.com>
2023-05-19 13:46:33 +0200

    welcome page and style.css created

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data = [
+        'page_title' => 'Welcome Page',
+        'description' => 'Click a link to change page',
+        'links' => [
+            'contacts',
+            'about-us',
+            'jerry',
+        ]
+    ];
+    return view('home' , $data);
 });
